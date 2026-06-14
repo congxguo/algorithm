b7a854e5ad86c7a5d5c888faade2a321b63eaeda
CC <cong@CCs-MacBook-Pro.local>
2026-06-14 20:42:24 +0800

    feat: permutation

---
+++ permutation/permutation.py
@@ -0,0 +1,23 @@
+def permutation(nums):
+    ans = []
+    n = len(nums)
+    
+    def backtrace(start):
+        if start == n:
+            ans.append(nums[:])
+            return
+        
+        for u in range(start, n):
+            nums[start], nums[u] = nums[u], nums[start]
+            backtrace(start + 1)
+            nums[start], nums[u] = nums[u], nums[start]
+
+    
+    backtrace(0)
+    print(f'{ans}')
+    return ans
+
+permutation([1, 2, 3, 4])
+
+
+
