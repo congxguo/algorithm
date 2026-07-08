de12930699cf6f64752efdca28d4e84d2c480afd
Cong Guo <cong.guo@smartnews.com>
2026-07-08 20:23:21 +0800

    feat: longest_zigzag

---
+++ recursion/longest_zig_zag.py
@@ -0,0 +1,41 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        
+
+def longest_zigzag(root):
+    ans = 0
+    
+    def dfs(node):
+        nonlocal ans
+        if not node:
+            return -1, -1
+        
+        left_left, left_right = dfs(node.left)
+        right_left, right_right = dfs(node.right)
+        
+        left_len = left_right + 1
+        right_len = right_left + 1
+        
+        ans = max(ans, left_len, right_len)
+        
+        return left_len, right_len
+    
+    dfs(root)
+    return ans
+    
+
+root = TreeNode(1)
+
+root.left = TreeNode(2)
+root.right = TreeNode(3)
+
+root.left.right = TreeNode(4)
+root.left.right.left = TreeNode(5)
+root.left.right.left.right = TreeNode(6)
+
+root.right.right = TreeNode(7)
+root.right.right.right = TreeNode(8)
+print(f'{longest_zigzag(root)}')
