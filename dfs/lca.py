ab7f1561419c0a71fd64dd3290cf7a1cc9d7acf7
Cong Guo <cong.guo@smartnews.com>
2026-07-09 11:33:31 +0800

    feat: lca

---
+++ dfs/lca.py
@@ -0,0 +1,60 @@
+class TreeNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+        
+def build_tree(level):
+    if not level or level[0] is None:
+        return None
+
+    root = TreeNode(level[0])
+    queue = [root]
+    i = 1
+
+    while queue and i < len(level):
+        node = queue.pop(0)
+
+        if i < len(level) and level[i] is not None:
+            node.left = TreeNode(level[i])
+            queue.append(node.left)
+        i += 1
+
+        if i < len(level) and level[i] is not None:
+            node.right = TreeNode(level[i])
+            queue.append(node.right)
+        i += 1
+
+    return root
+
+
+def lca(root, p, q):
+    if root is None or root == p or root == q:
+        return root
+    
+    
+    left = lca(root.left, p, q)
+    right = lca(root.right, p, q)
+    
+    if left and right:
+        return root
+    
+    return left if left else right
+
+
+def find(root, value):
+    if not root:
+        return None
+    if root.value == value:
+        return root
+    return find(root.left, value) or find(root.right, value)
+
+
+level_order = [1, None, 1, 1, 1, None, None, 1, 1, None, 1, None, None, None, 1]
+root = build_tree(level_order)
+
+p = find(root, 1)   # first 1 encountered
+q = find(root.right.right, 1)  # deeper 1
+
+lca = lca(root, p, q)
+print(lca.value)
